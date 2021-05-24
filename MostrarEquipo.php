2828a96ae9866fe12c4d756fac286bf176724cd8
Braylosky <fddf@live.com.mx>
2021-05-23 20:03:46 -0500

    Creacion Funcion de Mostar Equipo

---
+++ MostrarEquipo.php
@@ -0,0 +1,52 @@
+<?php 
+
+	$conexion=mysqli_connect('localhost','root','','BD');
+
+ ?>
+
+codigo, cantidad, descripcion, caracteristicas, marca, modelo, tipo, capacidad, vUtil
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mostrar datos</title>
+</head>
+<body>
+
+<br>
+
+	<table>
+		<tr>
+			<td>Código</td>
+			<td>Descripcion</td>
+			<td>Características</td>
+			<td>Marca</td>
+			<td>Modelo</td>	
+            <td>Tipo</td>
+            <td>Capacidad</td>
+            <td>Vida Útil</td>
+		</tr>
+
+		<?php 
+		$sql="SELECT * from t_Equipo";
+		$result=mysqli_query($conexion,$sql);
+
+		while($mostrar=mysqli_fetch_array($result)){
+		 ?>
+
+		<tr>
+			<td><?php echo $mostrar['Codigo'] ?></td>
+			<td><?php echo $mostrar['Descripcion'] ?></td>
+			<td><?php echo $mostrar['Caracteristicas'] ?></td>
+			<td><?php echo $mostrar['Marca'] ?></td>
+			<td><?php echo $mostrar['Modelo'] ?></td>
+            <td><?php echo $mostrar['Tipo'] ?></td>
+            <td><?php echo $mostrar['Capacidad'] ?></td>
+            <td><?php echo $mostrar['V_util'] ?></td>
+		</tr>
+	<?php 
+	}
+	 ?>
+	</table>
+
+</body>
+</html>
