57e6ef47db07c8bd92a0bf0e6009a7c3d084326c
Enrique Miramontes <Enalmiramontesti@ittepic.edu.mx>
2021-05-23 18:56:55 -0600

    Se corrigio errores de sintaxis del ModuloMatariales

--- ModuloMateriales.js
+++ ModuloMateriales.js
@@ -1,94 +1,94 @@
 class  moduloMateriales   {
     constructor ( codigo ,  cantidad ,  descripcion ,  caracteristicas ,  marca ,  area ,  tipo ,  peso ,  caducidad )  {
-            esto . _codigo  =  codigo
-            esto . _cantidad  =  cantidad
-            esto . _descripcion  =  descripcion
-            esto . _caracteristicas  =  caracteristicas
-            esto . _marca  =  marca
-            esto . _area  =  area
-            esto . _tipo  =  tipo
-            esto . _peso  =  peso
-            esto . _caducidad  =  caducidad
+            this . _codigo  =  codigo
+            this . _cantidad  =  cantidad
+            this . _descripcion  =  descripcion
+            this . _caracteristicas  =  caracteristicas
+            this . _marca  =  marca
+            this . _area  =  area
+            this. _tipo  =  tipo
+            this . _peso  =  peso
+            this. _caducidad  =  caducidad
         }
         // Codigo
-    set  scodigo ( c )  {
-        esto . _codigo  =  c
+    set  codigo ( c )  {
+        this. _codigo  =  c
     }
 
-    obtener  gcodigo ( )  {
-            devuelve  esto . _codigo
+    get  codigo ( )  {
+            return this. _codigo
         }
         // Cantidad prueba 
-    set  scantidad ( c )  {
-        esto . _cantidad  =  c
+    set  cantidad ( c )  {
+        this._cantidad  =  c
     }
 
-    obtener  gcantidad ( )  {
-            devuelve  esto . _cantidad
-        }
-        // Descripcion
-    set  sdescripcion ( c )  {
-        esto . _descripcion  =  c
+    get  cantidad ( )  {
+            return  this. _cantidad
+    }
+    // Descripcion
+    set  descripcion ( c )  {
+        this . _descripcion  =  c
     }
 
-    obtener  gdescripcion ( )  {
-            devuelve  esto . _descripcion
+    get  descripcion ( )  {
+        return this. _descripcion
         }
         // Caracteristicas
-    set  scaracteristicas ( c )  {
-        esto . _caracteristicas  =  c
+    set  caracteristicas ( c )  {
+        this. _caracteristicas  =  c
     }
 
-    obtener  gcaracteristicas ( )  {
-            devuelve  esto . _caracteristicas
+    get  caracteristicas ( )  {
+            return this. _caracteristicas
         }
         // marca
-    set  smarca ( c )  {
-        esto . _marca  =  c
+    set  marca ( c )  {
+        this. _marca  =  c
     }
 
-    obtener  gmarca ( )  {
-            devuelve  esto . _marca
+    get  marca ( )  {
+            return this. _marca
         }
         // area
-    establecer  sarea ( c )  {
-        esto . _area  =  c
+    set area ( c )  {
+        this. _area  =  c
     }
 
-    obtener  garea ( )  {
-            devuelve  esto . _area
+    get  area ( )  {
+            return  this. _area
         }
         // Tipo
-    set  stipo ( c )  {
-        esto . _tipo  =  c
+    set  tipo ( c )  {
+        this. _tipo  =  c
     }
 
-    obtener  gtipo ( )  {
-            devuelve  esto . _tipo
+    get  tipo ( )  {
+            return this. _tipo
         }
         // Capaciad
-    set  speso ( c )  {
-        esto . _peso  =  c
+    set  peso ( c )  {
+        this. _peso  =  c
     }
 
-    obtener  gpeso ( )  {
-            devuelve  esto . _peso
+    get  peso ( )  {
+            return  this._peso
         }
         // caducidadi
-    set  scaducidad ( c )  {
-        esto . _caducidad  =  c
+    set  caducidad ( c )  {
+        this. _caducidad  =  c
     }
 
-    obtener  gcaducidad ( )  {
-        devuelve  esto . _caducidad
+    get  caducidad ( )  {
+        return  this. _caducidad
     }
 
-    imprimir ( )  {
+    imprimir ()  {
         consola . log ( "Codigo:"  +  this . _codigo  +  "Cantidad:"  +  this . _cantidad  +  "Descripcion:"  +  this . descripcion  +
-            "Caracteristicas:"  +  esto . _caracteristicas  +  "marca:"  +  esto . _descripcion  +  "area:"  +  esto . _area  +
-            "Tipo:"  +  esto . _tipo  +  "peso:"  +  esto . _peso  +  "caducidad:"  +  esto . _caducidad )
+            "Caracteristicas:"  +  this . _caracteristicas  +  "marca:"  +  this . _descripcion  +  "area:"  +  this . _area  +
+            "Tipo:"  +  this . _tipo  +  "peso:"  +  this. _peso  +  "caducidad:"  +  this._caducidad )
     }
 }
 let  objetoEquipo  =  new  moduloEquipo ( '1111' ,  100 ,  'Mortero' ,  'secado rapido' ,  'Azul' ,  'Enjarres' ,  'sr' ,  50 ,  2 ) ;
 objetoEquipo . _caducidad  =  10
-objetoEquipo . imprimir ( )
+objetoEquipo . imprimir ()
