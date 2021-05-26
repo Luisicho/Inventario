b04100451ac53103125f0a596f69a4d9060d9b09
paulcampos56 <pcmessi56@gmail.com>
2021-05-26 01:38:06 -0500

    se agrego la funcion limpiar

--- limpiarCampos.js
+++ limpiarCampos.js
@@ -1,5 +1,13 @@
 function limpiar() {
     document.getElementById("CodigoM").value = "";
+    document.getElementById("NombreM").value = "";
+    document.getElementById("unidadM").value = "";
+    document.getElementById("cantidadM").value = "";
+    document.getElementById("descripM").value = "";
+    document.getElementById("modeloM").value = "";
+    document.getElementById("marcaM").value = "";
+    document.getElementById("tipoM").value = "";
+    
 
 
 }
